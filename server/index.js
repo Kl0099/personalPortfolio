5ae4e985ab829b6dafd624171e9b14921910fb78
Kl0099 <lodhakrunal464@gmail.com>
2024-04-02 11:21:15 +0530

    add url in backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mypersonalportfolio24.netlify.app",
+
     credentials: true,
   })
 );
